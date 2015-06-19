208659c0b02269e3989bafc4c7438efe1efced5d
Ros Corcoran <roscorcoran@gmail.com>
2015-06-20 00:16:48 +0100

    latest gulp-html-to-data-uri

--- Gulpfile.js
+++ Gulpfile.js
@@ -7,7 +7,8 @@ var gulp = require('gulp'),
     webserver = require('gulp-webserver'),
     htmlToDataURI = require('gulp-html-to-data-uri'),
     inline = require('gulp-inline'),
-    minifyCss = require('gulp-minify-css');
+    minifyCss = require('gulp-minify-css'),
+    eventStream = require('event-stream');
 
 //gulp.src('public/index.html')
 //    .pipe(inline({
@@ -24,7 +25,7 @@ var server = {
   }
 }
 
-var distDir = 'dist/';
+var distDir = './dist/';
 
 /*
  * Dist
@@ -39,10 +40,38 @@ gulp.task('build', function () {
         js: uglify(),
         css: minifyCss()
       }))
-      .pipe(htmlmin({comments:true}))
-      //.pipe(gulp.dest('dist/'))
-      .pipe(htmlToDataURI());
+      .pipe(htmlmin({comments: true}))
+      .pipe(htmlToDataURI())
+      .pipe(dataURItoFile())
+      .pipe(gulp.dest(distDir));
 });
+
+function dataURItoFile(opts) {
+  // you're going to receive Vinyl files as chunks
+  opts = opts || {};
+  opts.link = opts.link || {innerHTML: 'Here is your data URI link'};
+  opts.pre = opts.pre || true;
+  function transform(file, cb) {
+    // read and modify file contents
+    var contents = '<html><head></head><body><a href="'
+        + String(file.contents) + '">' + opts.link.innerHTML + '</a>';
+    if (opts.pre) {
+      contents = contents + '<pre style="word-wrap: break-word;">' + String(file.contents) + '</pre>';
+    }
+    contents = contents + '</body></html>';
+    file.contents = new Buffer(String(contents));
+
+    // if there was some error, just pass as the first parameter here
+    cb(null, file);
+  }
+
+  // returning the map will cause your transform function to be called
+  // for each one of the chunks (files) you receive. And when this stream
+  // receives a 'end' signal, it will end as well.
+  //
+  // Additionally, you want to require the `event-stream` somewhere else.
+  return eventStream.map(transform);
+}
 
 /*
  * Dev
@@ -56,16 +85,16 @@ gulp.task('watch', function () {
   })
 });
 
-//gulp.task('webserver:dev', ['build', 'watch'], function () {
-//  gulp.src(['./'])
-//      .pipe(webserver({
-//        host: '0.0.0.0',
-//        port: server.dev.port,
-//        livereload: true,
-//        directoryListing: false,
-//        open: 'http://localhost:' + server.dev.port + '/app/index.html'
-//      }));
-//});
+gulp.task('webserver:dev', ['build', 'watch'], function () {
+  gulp.src(['./'])
+      .pipe(webserver({
+        host: '0.0.0.0',
+        port: server.dev.port,
+        livereload: true,
+        directoryListing: false,
+        open: 'http://localhost:' + server.dev.port + '/dist/link.html'
+      }));
+});
 
 //Assume default is just a build
 gulp.task('default', ['build'], function () {
